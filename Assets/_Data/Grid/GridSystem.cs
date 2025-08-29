3cfed9eb3536133b057b3e51c79111f13dbc806b
LamLam <ailamtran1812@gmail.com>
2025-08-29 11:50:02 +0200

    Monoscript n Node n Gridsystem

---
+++ Assets/_Data/Grid/GridSystem.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GridSystem: PikaMonoBehaviourScript
+{
+    public List<Node> nodes;
+    public int width = 20;
+    public int height = 13;
+
+    protected override void LoadComponents()
+    {
+        base.LoadComponents();
+        this.InitGridSystem();
+    }
+    protected virtual void InitGridSystem()
+    {
+        if (this.nodes.Count > 0) return;
+        for (int x = 0; x < this.width; x++)
+        {
+            for (int y = 0; y < this.height; y++)
+            {
+                Node node = new Node()
+                {
+                    x = x,
+                    y = y
+                };
+                this.nodes.Add(node);
+            }
+        }
+    }
+}
---
+++ Assets/_Data/Grid/Node.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+[Serializable]
+public class Node
+{
+    public int x = 0;
+    public int y = 0;
+    public int weight = 1;
+    public bool occupied = false;
+    public Node up;
+    public Node left;
+    public Node right;
+    public Node down;
+}
