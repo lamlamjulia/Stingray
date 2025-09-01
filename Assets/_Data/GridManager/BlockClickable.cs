88f4624302d86823506c4795de17310703d907aa
LamLam <ailamtran1812@gmail.com>
2025-09-01 18:34:13 +0200

    added BlockClickable

---
+++ Assets/_Data/GridManager/BlockClickable.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(BoxCollider))] //to prevent sb to remove this component
+public class BlockClickable: BlockAbstract
+{
+    public BoxCollider _collider;
+    protected override void LoadComponents()
+    {
+        base.LoadComponents();
+        this.LoadCollider();
+    }
+    protected virtual void LoadCollider()
+    {
+        if (this._collider != null) return;
+        this._collider = GetComponent<BoxCollider>();
+        this._collider.isTrigger = true;
+        this._collider.size = new Vector3(0.7f, 0.9f, 0.5f);
+        Debug.LogWarning(transform.name + " LoadCollider", gameObject);
+    }
+    protected void OnMouseUp()
+    {
+        Debug.LogWarning(transform.name + " BlockClickable", gameObject);
+    }
+}
--- Assets/_Data/GridManager/GridManagerCtrl.cs
+++ Assets/_Data/GridManager/GridManagerCtrl.cs
@@ -2,9 +2,22 @@ using UnityEngine;
 
 public class GridManagerCtrl : PikaMonoBehaviour
 {
+    [Header("GridManagerCtrl ")]
+    private static GridManagerCtrl instance;
+    public static GridManagerCtrl Instance => instance;
+    public static string BLOCK = "Block";
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public BlockSpawner blockSpawner;
+    public BlockCtrl firstBlock;
+    public BlockCtrl lastBlock;
 
+    protected override void Awake()
+    {
+        base.Awake();
+        if (GridManagerCtrl.instance != null) Debug.LogError("Only 1 GridManagerCtrl  should exist");
+        GridManagerCtrl.instance = this;
+    }
     protected override void LoadComponents()
     {
         base.LoadComponents();
@@ -15,5 +28,22 @@ public class GridManagerCtrl : PikaMonoBehaviour
         if (this.blockSpawner != null) return;
         this.blockSpawner = transform.Find("BlockSpawner").GetComponent<BlockSpawner>();
         Debug.Log(transform.name + " LoadSpawner", gameObject); 
+    }
+    protected virtual void SetBlock(BlockCtrl blockCtrl)
+    {
+        if (this.firstBlock != null && this.lastBlock != null) 
+        {
+            this.firstBlock = null;
+            this.lastBlock = null;
+            Debug.LogWarning("Reset blocks");
+            return;
+        }
+        if (this.firstBlock == null)
+        {
+            this.firstBlock = blockCtrl;
+            return;
+        }
+
+        this.lastBlock = blockCtrl;
     }
 }
